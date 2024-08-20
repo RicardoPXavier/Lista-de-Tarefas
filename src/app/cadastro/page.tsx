4fd6b79e31ea8fdf75fe9f5e5ad60d977503e8b6
Ricardo Xavier <ricardodepaulaxavier@gmail.com>
2024-08-20 10:08:06 -0300

    Ajuste flexbox da tela de cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -4,14 +4,22 @@ import Image from 'next/image';
 import { doc, getDoc, updateDoc, addDoc, collection } from 'firebase/firestore';
 import { firestore } from "../../config/firebase";
 import style from '@/app/cadastro/page.module.scss'
+import email from '../../icons/email.svg'
+import senha from '../../icons/senha.svg'
+import telefone from '../../icons/telefone.svg'
+import username from '../../icons/username.svg'
 
 export default function TelaCadastro(){
     return(
         <main>
-            <div className={style.container}>
+            <div id={style.container}>
                 <div className={style.titulo}>Cadastrar</div>
-                <div>Nome
+                <div className={style.nomeTitulo}>Nome:
                     <div className={style.inputNome}>
+                        <Image className={style.username}
+                            src={username}
+                            alt="logo"
+                        />
                         <input type="text" 
                                placeholder="Username"
                                className={style.nome}
@@ -20,8 +28,12 @@ export default function TelaCadastro(){
                 </div>
 
 
-                <div>Email
+                <div className={style.emailTitulo}>Email:
                     <div className={style.inputEmail}>
+                        <Image className={style.email}
+                            src={email}
+                            alt="logo"
+                        />
                         <input type="text" 
                                placeholder="Email"
                                className={style.email}
@@ -29,8 +41,12 @@ export default function TelaCadastro(){
                     </div>
                 </div>
 
-                <div>Telefone
+                <div className={style.telefoneTitulo}>Telefone:
                     <div className={style.inputTelefone}>
+                        <Image className={style.telefone}
+                            src={telefone}
+                            alt="logo"
+                        />
                         <input type="text" 
                                placeholder="Telefone"
                                className={style.telefone}
@@ -38,8 +54,12 @@ export default function TelaCadastro(){
                     </div>
                 </div>
 
-                <div>Senha
+                <div className={style.senhaTitulo}>Senha:
                     <div className={style.inputSenha}>
+                        <Image className={style.senha}
+                            src={senha}
+                            alt="logo"
+                        />
                         <input type="text" 
                                placeholder="Senha"
                                className={style.senha}
