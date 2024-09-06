1d4474c429f9f90022cd484164d6266b350b74d4
Ricardo Xavier <ricardodepaulaxavier@gmail.com>
2024-09-06 20:02:23 -0300

    Pequenos ajustes na parte de design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,36 +4,39 @@ import googleIcon from '../../icons/google.svg';
 
 export default function TelaLogin() {
     return (
+
         <main>
-            <div id={style.container}>
-                <div className={style.titulo}>Bem Vindo(a)!</div>
-                <div className={style.inputGroup}>
-                    <label>Email:</label>
-                    <input type="email" placeholder="Digite seu email" className={style.input}/>
-                </div>
-                <div className={style.inputGroup}>
-                    <label>Senha:</label>
-                    <input type="password" placeholder="Digite sua senha" className={style.input}/>
-                </div>
-                <div className={style.options}>
-                    <div className={style.rememberMe}>
-                        <input type="checkbox" id="remember" className={style.checkbox}/>
-                        <label htmlFor="remember">Lembrar senha</label>
+            <div className={style.wrapper}>
+                <div id={style.container}>
+                    <div className={style.titulo}>Bem Vindo(a)!</div>
+                    <div className={style.inputGroup}>
+                        <label>Email:</label>
+                        <input type="email" placeholder="Digite seu email" className={style.input} />
                     </div>
-                    <a href="#" className={style.forgotPassword}>Esqueceu sua senha?</a>
-                </div>
-                <div className={style.containerButton}>
-                    <button className={style.buttonAcessar}>Acessar</button>
-                </div>
-                <div className={style.alternativeLogin}>
-                    <p>Ou entre com:</p>
-                    <button className={style.googleLogin}>
-                        <Image src={googleIcon} alt="Google" className={style.googleIcon}/>
-                    </button>
-                </div>
-                <hr className={style.separator} />
-                <div className={style.signupPrompt}>
-                    <p>Ainda não possui uma conta? <a href="#" className={style.signupLink}>Cadastre-se</a></p>
+                    <div className={style.inputGroup}>
+                        <label>Senha:</label>
+                        <input type="password" placeholder="Digite sua senha" className={style.input} />
+                    </div>
+                    <div className={style.options}>
+                        <div className={style.rememberMe}>
+                            <input type="checkbox" id="remember" className={style.checkbox} />
+                            <label htmlFor="remember">Lembrar senha</label>
+                        </div>
+                        <a href="#" className={style.forgotPassword}>Esqueceu sua senha?</a>
+                    </div>
+                    <div className={style.containerButton}>
+                        <button className={style.buttonAcessar}>Acessar</button>
+                    </div>
+                    <div className={style.alternativeLogin}>
+                        <p>Ou entre com:</p>
+                        <button className={style.googleLogin}>
+                            <Image src={googleIcon} alt="Google" className={style.googleIcon} />
+                        </button>
+                    </div>
+                    <hr className={style.separator} />
+                    <div className={style.signupPrompt}>
+                        <p>Ainda não possui uma conta? <a href="#" className={style.signupLink}>Cadastre-se</a></p>
+                    </div>
                 </div>
             </div>
         </main>
